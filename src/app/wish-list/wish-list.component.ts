929c28d50601714b2a038342a05786072cfca3da
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-13 23:59:47 +0300

    showing wishes

--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { Wish } from '../../shared/modules/Wish';
+import { WishCardComponent } from "../wish-card/wish-card.component";
 
 @Component({
-  selector: 'wish-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './wish-list.component.html',
-  styleUrl: './wish-list.component.css'
+    selector: 'wish-list',
+    standalone: true,
+    templateUrl: './wish-list.component.html',
+    styleUrl: './wish-list.component.css',
+    imports: [WishCardComponent]
 })
 export class WishListComponent {
  @Input() wishes: Wish[] = [];
