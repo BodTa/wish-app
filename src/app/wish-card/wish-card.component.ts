7c0814a479d86f78b07ad901a245244696a171dc
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-13 23:18:59 +0300

    initial wishcard

---
+++ src/app/wish-card/wish-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wish-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './wish-card.component.html',
+  styleUrl: './wish-card.component.css'
+})
+export class WishCardComponent {
+
+}
