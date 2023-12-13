50a3b509f3991e85ed30b4b317a613894cf331eb
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-13 23:43:19 +0300

    getting wishes from local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Wish } from '../shared/modules/Wish';
+import { WishFilterFormComponent } from "./wish-filter-form/wish-filter-form.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, WishFilterFormComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'wish-app';
+
+  wishList:  Wish[] =[];
+  getWishListFromLocalStorage() {
+    this.wishList = JSON.parse(localStorage.getItem('wishList') || '[]');
+  }
+
+  ngOnInit(): void {
+    this.getWishListFromLocalStorage();
+  }
 }
