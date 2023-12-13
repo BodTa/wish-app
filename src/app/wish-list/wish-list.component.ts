d2de25d7a66498b9330439016485ef042cb2b47e
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-13 23:16:24 +0300

    initial wish-list

---
+++ src/app/wish-list/wish-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wish-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './wish-list.component.html',
+  styleUrl: './wish-list.component.css'
+})
+export class WishListComponent {
+
+}
