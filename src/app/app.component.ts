fb8f7021c45312e0e9928049182d57ff8e80e1a2
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-14 00:56:08 +0300

    getting wishes from ls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,12 @@ export class AppComponent implements OnInit {
   title = 'wish-app';
 
   wishList:  Wish[] =[];
+  initialList: Wish[] = [
+    { id: 1, wish: 'I wish I could fly', createdAt: new Date(), completed: false },
+    { id: 2, wish: 'I wish I could be invisible', createdAt: new Date(), completed: false },
+    { id: 3, wish: 'I wish I could be a millionaire', createdAt: new Date(), completed: false },
+  ];
   ngOnInit(): void {
-    this.wishList = [
-      { id: 1, wish: 'I wish I could fly', createdAt: new Date(), completed: false },
-      { id: 2, wish: 'I wish I could be invisible', createdAt: new Date(), completed: false },
-      { id: 3, wish: 'I wish I could be a millionaire', createdAt: new Date(), completed: false },
-    ]};
+    this.wishList =localStorage.getItem('wishList') ? JSON.parse(localStorage.getItem('wishList') || '{}') : this.initialList;
+  }
 }
