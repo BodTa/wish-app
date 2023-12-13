83bb3f632df53bda9a7c0408ae440416b2b4f5f8
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-13 23:38:06 +0300

    wish filter form created

---
+++ src/app/wish-filter-form/wish-filter-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wish-filter-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './wish-filter-form.component.html',
+  styleUrl: './wish-filter-form.component.css'
+})
+export class WishFilterFormComponent {
+
+}
