82d194b1c2108acaadbe8ba5cc05cd821e29ba13
Taner Özdemir <95244060+BodTa@users.noreply.github.com>
2023-12-14 21:36:44 +0300

    Adding wishes

--- src/app/wish-add-form/wish-add-form.component.ts
+++ src/app/wish-add-form/wish-add-form.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'wish-add-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './wish-add-form.component.html',
   styleUrl: './wish-add-form.component.css'
 })
 export class WishAddFormComponent {
+  wish:string = '';
 
+  @Output() newWish = new EventEmitter<string>();
+
+  addWish(){
+    this.newWish.emit(this.wish);
+    this.wish = '';
+  }
 }
--- src/app/wish-list/wish-list.component.ts
+++ src/app/wish-list/wish-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Wish } from '../../shared/modules/Wish';
 import { WishCardComponent } from "../wish-card/wish-card.component";
+import { WishAddFormComponent } from "../wish-add-form/wish-add-form.component";
 
 @Component({
     selector: 'wish-list',
     standalone: true,
     templateUrl: './wish-list.component.html',
     styleUrl: './wish-list.component.css',
-    imports: [WishCardComponent]
+    imports: [WishCardComponent, WishAddFormComponent]
 })
 export class WishListComponent {
  @Input() wishes: Wish[] = [];
@@ -23,6 +24,16 @@ export class WishListComponent {
   }
  deleteWish(id: number) {
     this.wishes = this.wishes.filter(wish => wish.id !== id);
+    localStorage.setItem('wishList', JSON.stringify(this.wishes));
+  }
+
+  addNewWish(wish: string) {
+    this.wishes.push({
+      id: this.wishes.length + 1,
+      wish: wish,
+      completed: false,
+      createdAt: new Date()
+    });
     localStorage.setItem('wishList', JSON.stringify(this.wishes));
   }
 }
